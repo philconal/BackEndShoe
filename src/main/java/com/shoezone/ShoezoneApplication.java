2606cd88182609fcd1117ea845c7007663e9c713
Phicnq2411 <phicnq2411@gmail.com>
2021-07-23 14:53:57 +0700

    set up

---
+++ src/main/java/com/shoezone/ShoezoneApplication.java
@@ -0,0 +1,13 @@
+package com.shoezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoezoneApplication.class, args);
+	}
+
+}
